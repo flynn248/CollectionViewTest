59801189c9b37c69ae82348c4714a24c1a075295
Shane Flynn <sflynn248@gmail.com>
2022-08-07 19:12:41 -0500

    Add project files.

---
+++ CollectionViewTest/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace CollectionViewTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+    double x = 0;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+	{
+        switch (e.StatusType)
+        {
+            case GestureStatus.Started:
+                SlipNSlide.Text = "Panning";
+                break;
+            case GestureStatus.Running:
+                SlipNSlide.TranslationX = x + e.TotalX;
+                break;
+            case GestureStatus.Completed:
+                x = SlipNSlide.TranslationX;
+                SlipNSlide.Text = "Not Panning";
+                break;
+        }
+    }
+
+    private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        SelectedItem.Text = $"Selected: {e.CurrentSelection.First()}";
+    }
+}
+
